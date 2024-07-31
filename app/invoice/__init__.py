ce4f1606377af1913891f0440dda43c3a937ca8e
kamilov.2000 <a.kamilov@jowi.app>
2024-07-31 13:56:34 +0500

    add files to all types of invoices

---
+++ app/invoice/__init__.py
@@ -0,0 +1,57 @@
+import os
+from app.invoice.models import File, Invoice
+from app.invoice.schema import (
+    ExpenseSchema,
+    FileWebSchema,
+    InvoiceSchema,
+    ProductionSchema,
+    TransferSchema,
+)
+from app.utils.exc import ItemNotFoundError
+from app.utils.func import hash_image_save, token_required
+from app.utils.schema import ResponseSchema, TokenSchema
+from app.base import session
+from .bp import invoice as invoice_bp
+from app.invoice.expense.bp import expense as expense_bp
+from app.invoice.transfer.bp import transfer as transfer_bp
+from app.invoice.production.bp import production as production_bp
+
+
+def register_update_photos_route(bp, route, response_schema):
+    @bp.route(route, methods=["POST"])
+    @token_required
+    @bp.arguments(FileWebSchema, location="files")
+    @bp.arguments(TokenSchema, location="headers")
+    @bp.response(400, ResponseSchema)
+    @bp.response(200, response_schema)
+    def change_photo(cur_user, data, token, invoice_id):
+        invoice = Invoice.get_by_id(invoice_id)
+        photo_files = data.get("files")
+        array = []
+        for p_file in photo_files:
+            try:
+                path = hash_image_save(p_file, "invoice", invoice_id)
+                array.append(File(filename=p_file.filename, path=path))
+            except ItemNotFoundError:
+                pass
+                # return msg_response("Photo not found", False), 400
+        if invoice.files:
+            for file in invoice.files:
+                try:
+                    session.delete(file)
+                    os.remove(file.path)
+                except FileNotFoundError:
+                    pass
+        invoice.files = array
+        session.commit()
+        return invoice
+
+
+register_update_photos_route(invoice_bp, "/<invoice_id>/update_photos/", InvoiceSchema)
+register_update_photos_route(expense_bp, "/<invoice_id>/update_photos/", ExpenseSchema)
+register_update_photos_route(
+    transfer_bp, "/<invoice_id>/update_photos/", TransferSchema
+)
+register_update_photos_route(
+    production_bp, "/<invoice_id>/update_photos/", ProductionSchema
+)
--- app/invoice/bp.py
+++ app/invoice/bp.py
@@ -1,13 +1,11 @@
-import os
 from flask.views import MethodView
 from flask_smorest import Blueprint, abort
 
-from app.invoice.models import File, Invoice
-from app.invoice.schema import FileWebSchema, InvoiceQueryArgSchema, InvoiceSchema
+from app.invoice.models import Invoice
+from app.invoice.schema import InvoiceQueryArgSchema, InvoiceSchema
 from app.base import session
 from app.utils.exc import ItemNotFoundError
-from app.utils.func import hash_image_save, token_required
-from app.utils.schema import ResponseSchema, TokenSchema
+from app.utils.schema import TokenSchema
 
 
 invoice = Blueprint(
@@ -67,32 +65,3 @@ class InvoiceById(MethodView):
             Invoice.delete(invoice_id)
         except ItemNotFoundError:
             abort(404, message="Item not found.")
-
-
-@invoice.post("/<invoice_id>/update_photos/")
-@token_required
-@invoice.arguments(FileWebSchema, location="files")
-@invoice.arguments(TokenSchema, location="headers")
-@invoice.response(400, ResponseSchema)
-@invoice.response(200, InvoiceSchema)
-def change_photo(cur_user, data, token, invoice_id):
-    invoice = Invoice.get_by_id(invoice_id)
-    photo_files = data.get("files")
-    array = []
-    for p_file in photo_files:
-        try:
-            path = hash_image_save(p_file, "invoice", invoice_id)
-            array.append(File(filename=p_file.filename, path=path))
-        except ItemNotFoundError:
-            pass
-            # return msg_response("Photo not found", False), 400
-    if invoice.files:
-        for file in invoice.files:
-            try:
-                session.delete(file)
-                os.remove(file.path)
-            except FileNotFoundError:
-                pass
-    invoice.files = array
-    session.commit()
-    return invoice
