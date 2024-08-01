0c1cfd159100a0a56a61d9f663eb5424d30cde8e
kamilov.2000 <a.kamilov@jowi.app>
2024-08-01 22:25:31 +0500

    adds post comment route and publish route

--- app/invoice/__init__.py
+++ app/invoice/__init__.py
@@ -1,4 +1,7 @@
+from sqlalchemy.exc import SQLAlchemyError
+from flask import current_app
 import os
+from app.choices import InvoiceStatuses
 from app.invoice.models import File, Invoice
 from app.invoice.schema import (
     ExpenseSchema,
@@ -9,7 +12,7 @@ from app.invoice.schema import (
     TransferSchema,
 )
 from app.utils.exc import ItemNotFoundError
-from app.utils.func import hash_image_save, token_required
+from app.utils.func import hash_image_save, msg_response, token_required
 from app.utils.schema import ResponseSchema, TokenSchema
 from app.base import session
 from .bp import invoice as invoice_bp
@@ -51,18 +54,63 @@ def register_update_photos_route(bp, route, response_schema):
 def register_add_comment_route(bp, route):
     @bp.post(route)
     @token_required
-    @bp.arguemnts(InvoiceCommentSchema)
-    @bp.arguemnts(TokenSchema, location="headers")
+    @bp.arguments(InvoiceCommentSchema)
+    @bp.arguments(TokenSchema, location="headers")
     @bp.response(400, ResponseSchema)
     @bp.response(200, InvoiceCommentSchema)
     def add_comment(cur_user, data, token, invoice_id):
-        pass
+        try:
+            data.user_id = cur_user.id
+            session.add(data)
+            session.commit()
+        except SQLAlchemyError as e:
+            current_app.logger.error(str(e.args))
+            session.rollback()
+            return msg_response("Something went wrong", False), 400
+        return data
 
-register_update_photos_route(invoice_bp, "/<invoice_id>/update_photos/", InvoiceSchema)
-register_update_photos_route(expense_bp, "/<expense_id>/update_photos/", ExpenseSchema)
-register_update_photos_route(
-    transfer_bp, "/<transfer_id>/update_photos/", TransferSchema
-)
-register_update_photos_route(
-    production_bp, "/<production_id>/update_photos/", ProductionSchema
-)
+
+def register_publish_invoice_route(bp, route):
+    @bp.post(route)
+    @token_required
+    @bp.arguments(TokenSchema, location="headers")
+    @bp.response(400, ResponseSchema)
+    @bp.response(200, ResponseSchema)
+    def publish_invoice(cur_user, token, invoice_id):
+        invoice = Invoice.get_by_id(invoice_id)
+        invoice.status = InvoiceStatuses.PUBLISHED
+        try:
+            session.commit()
+        except SQLAlchemyError as e:
+            current_app.logger.error(str(e.args))
+            session.rollback()
+            return msg_response("Something went wrong", False), 400
+        return msg_response("ok")
+
+
+def reg_invoice_routes():
+    bps = [
+        invoice_bp,
+        expense_bp,
+        transfer_bp,
+        production_bp,
+    ]
+    id_labels = [
+        "invoice_id",
+        "expense_id",
+        "transfer_id",
+        "production_id",
+    ]
+    schemas = [
+        InvoiceSchema,
+        ExpenseSchema,
+        TransferSchema,
+        ProductionSchema,
+    ]
+    for bp, label, schema in zip(bps, id_labels, schemas):
+        register_update_photos_route(bp, f"/<{label}>/update_photos/", schema)
+        register_add_comment_route(bp, f"/<{label}>/add_comment/")
+        register_publish_invoice_route(bp, f"/<{label}>/publish/")
+
+
+reg_invoice_routes()
